ea943377da129859bba18b341858d2d80a79a7ab
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-23 09:08:29 +0200

    fix #2 Programme Code PIN v2 commenté.

--- src/main/kotlin/tp03/activite02/02_CodePinV2.kt
+++ src/main/kotlin/tp03/activite02/02_CodePinV2.kt
@@ -1,9 +1,9 @@
 package tp03.activite02
 
-// Le programme principal se trouve dans le corps de la fonction ..........
+// Le programme principal se trouve dans le corps de la fonction main()
 
 // Quel est le but du programme principal ?
-// ...
+// Indiquer si le code saisi est valide ou non
 
 fun main() {
     println("Saisie ton code compris entre 0 et 9999 : ")
@@ -16,7 +16,8 @@ fun main() {
 }
 
 // À quelle condition le code est il valide ?
-// ...
+// Il faut que le code soit un nombre entier entre 0 et 9999 et
+// que la somme de ses chiffres soit égale à 10
 fun codeValide(code: Int): Boolean {
     if (code < 0 || code > 9999) {
         return false
@@ -25,7 +26,8 @@ fun codeValide(code: Int): Boolean {
 }
 
 // Quel est le but de cette fonction ?
-// ...
+// Calcule la somme des chiffres de l'objet référencé par code et
+// retourne la somme au programme appelant
 fun sommeChiffres(code: Int): Int {
     val unites = code % 10
     val dizaines = (code / 10) % 10
@@ -36,4 +38,5 @@ fun sommeChiffres(code: Int): Int {
 
 // Entre 01_CodePinV1 et 02_CodePinV2, quelle version du programme préférez-vous ?
 // Justifiez votre réponse.
-// ...
+// Le code est plus facile à comprendre, il est structuré en petites fonctions elles même faciles
+// à comprendre
