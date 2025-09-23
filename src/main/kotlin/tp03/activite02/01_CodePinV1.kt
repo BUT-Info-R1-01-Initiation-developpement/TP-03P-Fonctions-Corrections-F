f4ba20353a9f1ce282d96620cad6455bf6304202
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-23 09:08:17 +0200

    fix #1 Programme Code PIN v1 commenté.

--- src/main/kotlin/tp03/activite02/01_CodePinV1.kt
+++ src/main/kotlin/tp03/activite02/01_CodePinV1.kt
@@ -1,11 +1,12 @@
 package tp03.activite02
 
-// Le programme principal se trouve dans le corps de la fonction ..........
+// Le programme principal se trouve dans le corps de la fonction main()
 
 // Quel est le but du programme principal ?
-// ...
+// Vérifier si le code est correct
 // À quelle condition le code est valide ?
-// ...
+// Il faut que le code soit un nombre entier compris entre 0 et 9999 et
+// que la somme des chiffres soit égale à 10
 fun main() {
     println("Saisie ton code compris entre 0 et 9999 : ")
     val code = readln().toInt()
