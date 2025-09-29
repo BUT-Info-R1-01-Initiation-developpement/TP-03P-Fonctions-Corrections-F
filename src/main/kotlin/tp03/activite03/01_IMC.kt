3b7ac4fb085e85f75aeb7c4c3bf2e1c6e68fa005
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-29 16:21:13 +0200

    fix #5 Programme IMC amélioré. variante

--- src/main/kotlin/tp03/activite03/01_IMC.kt
+++ src/main/kotlin/tp03/activite03/01_IMC.kt
@@ -9,15 +9,12 @@ fun main() {
 
 fun affichageMessagePourImc(imc: Double) {
     println("L'IMC (Indice de masse corporelle) correspondant est de : $imc.")
-    if (imc < 18.5) {
-        println("Cet IMC correspond à une insuffisance pondérale.")
-    } else if (imc < 25.0) {
-        println("Cet IMC correspond à une corpulence normale.")
-    } else if (imc < 30.0) {
-        println("Cet IMC correspond à du surpoids.")
-    } else {
-        println("Cet IMC correspond à de l'obésité.")
-    }
+    println(when {
+        imc < 18.5 -> "Cet IMC correspond à une insuffisance pondérale."
+        imc < 25.0 -> "Cet IMC correspond à une corpulence normale."
+        imc < 30.0 -> "Cet IMC correspond à du surpoids."
+        else -> "Cet IMC correspond à de l'obésité."
+    })
 }
 
 fun calculeIMC(poids: Double, taille: Double): Double {
