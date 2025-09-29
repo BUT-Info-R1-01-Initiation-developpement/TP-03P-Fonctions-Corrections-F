73e8f0d226dbd1065784355d95ff43128a583db6
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-29 15:55:22 +0200

    fix #4 Programme IMC commenté.

--- src/main/kotlin/tp03/activite03/01_IMC.kt
+++ src/main/kotlin/tp03/activite03/01_IMC.kt
@@ -1,11 +1,18 @@
 package tp03.activite03
 
 fun main() {
+    // Récupère le poids saisi par l'utilisateur
     println("Entre un poids en kg :")
     val poids = readln().toDouble()
+
+    // Récupère la taille saisie de l'utilisateur
     println("Entre une taille en m : ")
     val taille = readln().toDouble()
+
+    // Calcule de l'IMC
     val imc = poids / (taille * taille)
+
+    // Affichage du message adapté en fonction de la valeur de l'IMC
     println("L'IMC (Indice de masse corporelle) correspondant est de : $imc.")
     if (imc < 18.5) {
         println("Cet IMC correspond à une insuffisance pondérale.")
