708b15cefd55e53367e651c927e1820a03c320d1
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-30 09:11:12 +0200

    fix #8 Programme ChiFuMi sous forme commentaires.

---
+++ src/main/kotlin/tp03/activite04/01_Chifumi.kt
@@ -0,0 +1,10 @@
+package tp03.activite04
+
+
+fun main() {
+    // Tant que le joueur veut jouer
+    //  - récupère la figure du joueur
+    //  - Récupère la figure de l'ordinateur
+    //  - Détermine le vainqueur de la partie
+    //  - Affiche les informations sur le vainqueur.
+}
