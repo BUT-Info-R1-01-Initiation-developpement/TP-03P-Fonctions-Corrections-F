340167e778c68d887b395654d87fe718d2a63424
Franck Silvestre <franck.silvestre@gmail.com>
2025-10-07 13:37:59 +0200

    fix #10 Programme ChiFuMi v2.

--- src/main/kotlin/tp03/activite04/01_Chifumi.kt
+++ src/main/kotlin/tp03/activite04/01_Chifumi.kt
@@ -2,6 +2,21 @@ package tp03.activite04
 
 import kotlin.random.Random
 
+// Le mot clé const indique au compilateur qu'il peut remplacer la chaine de caractère dès la
+// compilation quand il tombe sur une expression mobilisant la constante !
+
+// Une val déclarée const est une val dont la valeur est connue dès la compilation.
+// L'indiquer explicitement au compilateur permet d'optimiser l'exécution du programme :
+// il n'y a plus d'opération de substitution à l'exécution car elles ont déjà été faites
+// à la compilation.
+
+// Les constantes peuvent être déclarées à l'extérieur de toute fonction pour les rendre globales,
+// c'est-à-dire accessible depuis n'importe quelle fonction.
+
+const val PIERRE = "Pierre"
+const val FEUILLE = "Feuille"
+const val CISEAUX = "Ciseaux"
+
 fun main() {
     afficheMessageBienvenue()
     do {
@@ -26,9 +41,9 @@ fun recupereFigureJoueur(): String {
     println(
         """
         Saisie ta figure :
-        1. Pierre
-        2. Feuille
-        3. Ciseaux
+        1. ${PIERRE}
+        2. ${FEUILLE}
+        3. ${CISEAUX}
     """.trimIndent()
     )
     var choix: String
@@ -45,11 +60,12 @@ fun choixInvalide(choix: String): Boolean {
     return choix != "1" && choix != "2" && choix != "3"
 }
 
+
 fun figurePourChoix(choix: String): String {
     return when (choix) {
-        "1" -> "Pierre"
-        "2" -> "Feuille"
-        else -> "Ciseaux"
+        "1" -> PIERRE
+        "2" -> FEUILLE
+        else -> CISEAUX
     }
 }
 
@@ -75,9 +91,9 @@ fun afficheInformationsSurLeVainqueur(figureJoueur: String, figureOrdinateur: St
 
 fun joueurGagne(figureJoueur: String, figureOrdinateur: String): Int {
     return when {
-        figureJoueur == "Pierre" && figureOrdinateur == "Ciseaux" -> 1
-        figureJoueur == "Feuille" && figureOrdinateur == "Pierre" -> 1
-        figureJoueur == "Ciseaux" && figureOrdinateur == "Feuille" -> 1
+        figureJoueur == PIERRE && figureOrdinateur == CISEAUX -> 1
+        figureJoueur == FEUILLE && figureOrdinateur == PIERRE -> 1
+        figureJoueur == CISEAUX && figureOrdinateur == FEUILLE -> 1
         figureJoueur == figureOrdinateur -> 0
         else -> -1
     }
